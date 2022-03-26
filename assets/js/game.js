2b23fe9be230226372ffa1d8de9125cc3d52941a
Peej <clawver30@gmail.com>
2022-03-26 14:51:28 -0600

    fixing playerName bugs

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,9 +169,22 @@ var shop = function() {
 
 /* GAME INFORMATION / VARIABLES */
 
+// Function to set name
+var getPlayerName = function() {
+  var name = "";
+
+// add loop here with prompt and condition
+  while (name === "" ||  name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
